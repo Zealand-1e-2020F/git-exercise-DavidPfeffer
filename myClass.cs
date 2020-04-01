ea35bd06411080fcd3de6a3f97e03d6b0a0f5554
David Pfeffer <davidpfeffer@hotmail.dk>
2020-04-01 11:10:06 +0200

    addet properties i myClass

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,20 @@ namespace GitExercise
 {
     class myClass
     {
+        string _name;
+        string _addresse; 
+        int _postnr;
+        public int MyProperty { get; set; }
         
+        public myClass(string name, string addresse, int postnr)
+        {
+            _name = name;
+            _addresse = addresse;
+            _postnr = postnr;
+        }
+
+        public string name { get => _name;}
+        public string addresse { get => _addresse;}
+        public int postnr { get => _postnr;}
     }
 }
